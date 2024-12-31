71e00e23640ae874d52750d9d98e3ea34a182962
lyssom <569810240@qq.com>
2024-12-31 17:33:43 +0800

    add gzip

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, send_from_directory, jsonify, render_template, request
+from flask import Flask, send_from_directory, jsonify, render_template, request, send_file, Response
 from flask_cors import CORS
-from gis_operator.space_distance import get_space_distance
+# from gis_operator.space_distance import get_space_distance
 import os
 
 app = Flask(__name__)
 CORS(app)  # 允许跨域
 
 # 瓦片文件夹路径
-TILES_DIR = os.path.abspath("D:/新建文件夹/terrain")  # 替换为你的瓦片文件夹路径
+TILES_DIR = os.path.abspath("./datas/example/fy_dem/terrain")  # 替换为你的瓦片文件夹路径
 
 @app.route('/')
 def serve_index():
@@ -30,7 +30,10 @@ def get_tile(z, x, y):
         print(6667)
         return jsonify({"error": "Tile not found"}), 404
     dtile = os.path.join(TILES_DIR, str(z), str(x))
-    return send_from_directory(dtile, f"{y}.terrain")
+    response = send_from_directory(dtile, f"{y}.terrain")
+    response.headers['Content-Encoding'] = 'gzip'  # 添加 gzip 编码响应头
+    response.headers['Content-Type'] = 'application/octet-stream'
+    return response
 
 # 提供瓦片文件
 @app.route('/tiles/layer.json')
@@ -67,6 +70,40 @@ def getSpaceDistance():
 def serve_tile(z, x, y):
     """服务瓦片数据"""
     return send_from_directory(f'./datas/pm25/{z}/{x}', f"{y}.png")
+
+
+from tools.test_db import get_data
+
+@app.route('/all/<int:z>/<int:x>/<int:y>.jpg')
+def serve_all_tile(z, x, y):
+    """服务瓦片数据"""
+    print (z, x, y)
+    d = get_data(z, x, y)
+    # print(d)
+    # # return send_from_directory(f'./datas/pm25/{z}/{x}', f"{y}.png")
+    # import sqlite3
+
+    # # 连接SQLite数据库
+    # conn = sqlite3.connect('../datas/-04-02-All.db')
+    # cursor = conn.cursor()
+
+    # # 执行查询，获取BLOB数据
+    # # cursor.execute("show databases")
+    # cursor.execute("SELECT DataValue FROM ImgTable LIMIT 1")
+    # # data = cursor.fetchone()[0]  # 获取BLOB数据
+
+    # # # 将BLOB数据写入文件
+    # # # with open('output_data.jpg', 'wb') as f:
+    # # #     f.write(data)
+
+    # # conn.close()
+    # with open('./output_data.jpg') as d:
+    #     d= d
+    #     print(333)
+    #     print(d)
+    return Response(d, mimetype='image/jpeg')
+    # image_path = './output_data.jpg'
+    # return send_file(image_path, mimetype='image/jpeg')
 
 
 # 启动服务器
